1292dce9379a960855fe76896891db95dc301305
Joshua <scott.joshua@gmail.com>
2017-12-08 10:45:15 -0700

    inital overide

--- index.js
+++ index.js
@@ -1,7 +1,155 @@
 'use strict';
 
-const awsServerlessExpress = require('aws-serverless-express')
-const app = require('./app')
-const server = awsServerlessExpress.createServer(app)
+const AWS = require("aws-sdk");
+AWS.config.update({region: "us-west-2"});
+const docClient = new AWS.DynamoDB.DocumentClient();
+const fetch = require("node-fetch");
 
-exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context);
+
+exports.handler = (event, context, callback) => {
+
+    let sku = event['pathParameters']['SKU'];
+    let countryCode = event['pathParameters']['CountryCode']
+    ;
+    const done = (err, res) => callback(null, {
+        statusCode: err ? '400' : '200',
+        body: err ? err.message : JSON.stringify(res),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    getSku(sku, countryCode, done);
+};
+
+
+exports.create = function (event, context, callback) {
+    let sku = event.sku;
+    let countryCode = event.countryCode;
+    loadSKU(sku, countryCode, done);
+
+    /*
+    USE TO CLEAR CACHE IF USING GATEWAY CACHE
+    var params = {
+        restApiId: 'atpCheck',
+        stageName: 'stage'
+    };
+    apigateway.flushStageAuthorizersCache(params, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else     console.log(data);           // successful response
+    });
+    */
+
+};
+
+
+exports.updateSKU = function(event, context, callback) {
+    let message = event.Records[0].Sns.Message;
+    console.log('Message received from SNS:', message);
+    loadSKU(message.sku, message.countryCode, callback);
+};
+
+
+
+exports.allocate = (event, context, callback) => {
+
+    let params = {
+        TableName:"Inventory",
+        Key: {
+            SKU : event.sku,
+            CountryCode: event.countryCode,
+        },
+        UpdateExpression: "set qty = qty - :requestQuantity",
+        ConditionExpression: "qty > :requestQuantity",
+        ExpressionAttributeValues:{
+            ":requestQuantity":event.requestedQuantity
+        },
+        ReturnValues:"ALL_NEW"
+    };
+
+    docClient.update(params,function(err, data){
+        console.log("Return from update", data);
+
+        console.log("now:", Date.now() - data.Attributes.timestamp);
+
+
+        if(Date.now() > data.Attributes.timestamp + 60000){
+            console.log("Time to update inventory");
+            let sns = new AWS.SNS();
+
+            sns.publish({
+                Message: JSON.stringify(data.Attributes),
+                TopicArn: SNS_TOPIC_ARN
+            }, function(err, data) {
+                if (err) {
+                    console.log("ERRIR" + err.stack);
+                    return;
+                }
+                console.log('push sent');
+                console.log(data);
+                context.done(null, 'Function Finished!');
+            });
+        }
+
+    });
+};
+
+
+
+const loadSKU = function(sku, countryCode, callback) {
+    let body = {"CountryCode":countryCode,"Region":"","Item":[{"SKU":sku,"RequestedQuantity":"1"}]};
+
+    fetch('https://www.nuskin.com/shop-service/api/v1/inventory/atp', {
+        method: 'POST',
+        body:    JSON.stringify(body),
+        headers: { 'Content-Type': 'application/json' , 'client_id':'735b1eb810304bba966af0891ab54053', 'client_secret':'0deea37049e84157A406C0C01D29F300'},
+    })
+        .then((response) => {
+            if (response.ok) {
+                return response;
+            }
+            return Promise.reject(new Error(
+                `Failed to fetch ${response.url}: ${response.status} ${response.statusText}`));
+        })
+        .then(response => response.buffer())
+        .then((buffer) => {
+
+                const product = JSON.parse(buffer);
+                docClient.put({
+                    TableName: 'Inventory',
+                    Item: {SKU: sku, CountryCode: countryCode, qty: product.ATPCheckResponse.Item[0].TotalAvailableQuantity, timestamp: Date.now()}
+                },   function(err, data) {
+                    if (err) {
+                        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+                    } else {
+                        console.log("Added item:", JSON.stringify(data, null, 2));
+                        callback(err, data);
+                    }
+                });
+            }
+        )
+};
+
+
+const getSku = function(sku, countryCode, callback) {
+    docClient.get({
+        TableName: 'Inventory',
+        Key: {SKU: sku, CountryCode: countryCode}
+    },  function(err, data) {
+        if(!data.Item){
+            exports.loadSKU(sku, countryCode, function(err, data){
+                if(!err){
+                    exports.get(sku, countryCode, callback)
+                }else{
+                    callback(err, data); //error loading SKU from SAP
+                }
+            });
+        }else{
+            callback(err, data);
+        }
+    });
+};
+
+
+
+
